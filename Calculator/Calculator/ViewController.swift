cdedfa3a0f67fc591331371189f9b45d9272ae73
lindongjie <lindj0307@gmail.com>
2016-08-23 22:24:01 +0800

    finish Calculator
    finish Calculator

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,6 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    // MARK: - Variables
+    private var userIsInTheMiddleOfTyping = false
+    
+    // MARK: - Properties
+    @IBOutlet weak var display: UILabel!
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - TableView Data Source
+    
+    // MARK: - TableView Delegate
+    
+    // MARK: - Custom Delegate
+    
+    // MARK: - Event Response
+    
+    // MARK: - Private Methods
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain: CalculatorBrain = CalculatorBrain()
+    
+    @IBAction func performOperation(sender: UIButton) {
+//        if let matchematicalSymbol = sender.currentTitle {
+//            userIsInTheMiddleOfTyping = false
+//            if matchematicalSymbol == "♊︎" {
+//                displayValue = M_PI
+//            } else if matchematicalSymbol == "✓" {
+//                displayValue = sqrt(displayValue)
+//            }
+//        }
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        userIsInTheMiddleOfTyping = false
+        if let matchicalSymbol = sender.currentTitle {
+            brain.performOperation(matchicalSymbol)
+        }
+        displayValue = brain.result
+    }
+    
+    // MARK: - Navigation
+    
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
